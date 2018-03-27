fcb5ce8879616ed9cce2c8cf6c344e8bd13c1e21
Vincent RAGOT <vincent.ragot@epitech.eu>
2018-03-27 14:47:17 +0200

    update home backend

--- EpitechEisenhower/EpitechEisenhower/Home/HomeViewController.swift
+++ EpitechEisenhower/EpitechEisenhower/Home/HomeViewController.swift
@@ -13,26 +13,41 @@ import Alamofire
 class HomeViewController: UICollectionViewController {
 
 	var presenter: HomePresenterImp?
+	private var displayResults = [TaskModel]()
 	
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.loadData()
     }
     
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return content.count
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell: UICollectionViewCell!
-        cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellReuseId", for: indexPath)
-        let customCell = cell as? CustomCell
-        let tmpModel = content[indexPath.row]
-        customCell?.myTitle.text = tmpModel.titre
-        return cell
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(indexPath)
-    }
+    extension HomeViewController: UICollectionViewDataSource {
+		func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+			return displayResults.count
+		}
+		
+		func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+			let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell",
+														  for: indexPath)
+			if let cell = cell as? ResultCollectionViewCell {
+				cell.setModel(model: displayResults[indexPath.row])
+			}
+			return cell
+		}
+	}
+
+	extension HomeViewController: UICollectionViewDelegate {
+		func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+			presenter?.cellWasSelected(indexPath: indexPath)
+		}
+	}
+
+	extension HomeViewController: HomeView {
+		func receiveData(results: [TaskModel]) {
+			displayResults = results
+			collectionView.reloadData()
+		}
+		func displayError(error: String) {
+			print(error)
+		}
+	}
 }
